79474ee566d3c836f508e7e10a1189ea0a6b2718
lozam <loza_maicol@hotmail.com>
2014-10-01 10:56:54 +0200

    https://github.com/lozam/uno.git

---
+++ WebApplication6/scripts/JavaScript1.js
@@ -0,0 +1,22 @@
+﻿var array1 = new Array(4);
+var array2 = [];
+var array3 = [1, 2, 6, 8, 7];
+
+array2[0] = "Pepe";
+array1[0] = "Eva";
+
+array3 = array3.concat(array2);
+array3.push("hola don Pepito");
+
+array3.splice(2,0, "adios", "don", "jose");
+
+for (i = 0; i < array3.length; i++){
+    document.write(array3[i] + "<br />");
+}
+alert(i);
+
+var hola = function (msg) {
+    alert('Hola ' + msg);
+};
+
+hola('Mundo');
---
+++ WebApplication6/scripts/funciones.js
@@ -0,0 +1,25 @@
+﻿function hola1(msg) {
+    alert(msg);
+}
+
+var hola2 = function (msg) {
+    alert(msg);
+}
+
+hola1("1, Dime el numero 1");
+hola2("2, Dime el numero 1");
+
+//Hola2 = "hola don pepeito";
+hola3 = hola2;
+
+hola3("hola");
+
+var pasar = function (objeto) {
+    alert(objeto);
+};
+
+var recibir = function (pasado) {
+    pasado("estoy llamando a una funcion desde otra");
+};
+
+recibir(pasar);
