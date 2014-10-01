5a501f7807cce17efb971f8fc6d41c870fa5e979
lozam <loza_maicol@hotmail.com>
2014-10-01 12:26:05 +0200

    nuevo2

---
+++ WebApplication6/scripts/operaciones.js
@@ -0,0 +1,21 @@
+﻿function sumar(n1, n2){
+    return n1 + n2;
+}
+
+(function () {
+    var r = sumar(15, 20);
+    alert(r);
+
+    var obj = { x: 22, y: 44 }; //json
+    //var obj = {resultado:0}; //json
+       
+    var args = [5, 23];
+
+    var rr = sumar.apply(obj, args);
+    var rrr = sumar.call(obj, 5, 23);
+
+    alert("con apply: "+rr);
+    alert("con call: " + rrr);
+
+    //alert(obj.resultado);
+})();
