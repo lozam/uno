36ca8b743df3b457e81c6bad35d03a3789da7594
lozam <loza_maicol@hotmail.com>
2014-10-02 12:31:15 +0200

    nuevos doc añadidos

---
+++ WebApplication6/scripts/manejoJS.js
@@ -0,0 +1,26 @@
+﻿function cambiarVisibilidad() {
+
+    var capas = document.getElementsByTagName("div");
+
+    for (var i = 0; i < capas.length; i++) {
+        if (capas[i].style.display == "none") {
+            capas[i].style.display = "block"
+        } else {
+            capas[i].style.display = "none"
+        }
+    }
+
+}
+
+function escribirTexto() {
+    var capa = document.getElementById("capa1");
+    capa.innerHTML += "Estoy escribiendo dentro de la capa<br />";
+}
+
+function cambiarPosicion() {
+    var texto = document.getElementById("posicion").value;
+    var capa = document.getElementById("capa2");
+    if (!isNaN(texto)) {
+        capa.style.left = texto + "px";
+    }
+}
---
+++ WebApplication6/scripts/objetos.js
@@ -0,0 +1,53 @@
+﻿var obj = new Object;
+obj.nombre = "luis";
+obj.apellido = "gil";
+
+var obj2 = { nombre: "luis", apeellido: "gil" };
+
+var obj3 = {};
+obj3.nombre = "luis";
+obj3.apellido = "gil";
+
+function Persona(nombre, apellido) {
+    this.nombre = nombre || "";
+    this.apellido = apellido || "";
+    this.escribir = function () {
+        alert(this.nombre+ " "+ this.apellido);
+    }
+}
+var p = new Persona("luis", "gil");
+var p2 = new Persona();
+var p3 = new Persona(undefined, "gil");
+
+p3.escribir();
+
+String.prototype.dimeAlgo = function () {
+    return "Algo";
+}
+String.prototype.toJSON = function () {
+    return "JSON";
+}
+var hola = "";
+alert(hola.dimeAlgo());
+alert(hola.toJSON());
+//-----------------------------------------------------
+var hola = "";
+
+alert(hola.toJSON());
+
+Estudiante.prototype = new Persona;
+Estudiante.constructor = Estudiante;
+
+function Estudiante(nombre, apellido, curso) {
+    this.nombre = nombre
+    this.apellido = apellido;
+    this.curso = curso;
+}
+
+Estudiante.prototype.escribir = function (){
+    alert(this.nombre + " " + this.apellido + " " + this.curso);
+};
+
+var e = new Estudiante("Luis", "Gil", "C#");
+alert(e instanceof Persona);
+e.escribir();
